0789acb6265db4607cbe4375f497e13d3cc114b9
sompoch promkaew <pro_poch@sompochs-MacBook-Pro.local>
2020-09-20 13:03:27 +0700

    changed app text

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Change from fetaure branch
         </a>
       </header>
     </div>
